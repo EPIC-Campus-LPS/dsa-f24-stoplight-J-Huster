0c9e32185c315cedfecd180a3f3270f3cdc6740a
J-Huster <139395235+J-Huster@users.noreply.github.com>
2024-10-11 09:46:23 -0600

    Add python files for part I and II
    Add python file for three LEDs stoplight
    Add python file for RGB LED stoplight

---
+++ led_stoplight.py
@@ -0,0 +1,69 @@
+#Importing GPIO and time libraries so the code can work with the GPIO 
+# pins and have the light turn on for certain amounts of time
+import RPi.GPIO as GPIO
+import time
+#Tells the program which naming conventions to use for the GPIO pins
+GPIO.setmode(GPIO.BCM)
+
+#Creates variable to store the number of the GPIO pin needed
+num = 0
+#Creates the variable that will store a value to deturmine how many 
+# times the program repeats
+n = 0
+while n < 2:
+	#Green LED for 5 seconds
+	#Assigns the variable the value of the number for the GPIO pin 
+	# needed to turn on the correct LED
+	num = 5
+	GPIO.setwarnings(False)
+	#Tells the program to use the GPIO pin as output
+	GPIO.setup(num,GPIO.OUT)
+	#Prints text in the teriminal that tells the user the LED is on
+	print("Green LED on")
+	#Sends power to the GPIO pin, turning the LED on
+	GPIO.output(num,GPIO.HIGH)
+	#Pauses the program for a certain amount of time
+	time.sleep(5)
+	#Prints text in the teriminal that tells the user the LED is off
+	print("Green LED off")
+	#Stops sending power to the GPIO pin, turning the LED off
+	GPIO.output(num,GPIO.LOW)
+	
+	#Yellow LED for 1 second
+	#Assigns the variable the value of the number for the GPIO pin 
+	# needed to turn on the correct LED
+	num = 22
+	GPIO.setwarnings(False)
+	#Tells the program to use the GPIO pin as output
+	GPIO.setup(num,GPIO.OUT)
+	#Prints text in the teriminal that tells the user the LED is on
+	print("Yellow LED on")
+	#Sends power to the GPIO pin, turning the LED on
+	GPIO.output(num,GPIO.HIGH)
+	#Pauses the program for a certain amount of time
+	time.sleep(1)
+	#Prints text in the teriminal that tells the user the LED is off
+	print("Yellow LED off")
+	#Stops sending power to the GPIO pin, turning the LED off
+	GPIO.output(num,GPIO.LOW)
+	
+	#Red LED for 4 seconds
+	#Assigns the variable the value of the number for the GPIO pin 
+	# needed to turn on the correct LED
+	num = 4
+	GPIO.setwarnings(False)
+	#Tells the program to use the GPIO pin as output
+	GPIO.setup(num,GPIO.OUT)
+	#Prints text in the teriminal that tells the user the LED is on
+	print("Red LED on")
+	#Sends power to the GPIO pin, turning the LED on
+	GPIO.output(num,GPIO.HIGH)
+	#Pauses the program for a certain amount of time
+	time.sleep(4)
+	#Prints text in the teriminal that tells the user the LED is off
+	print("Red LED off")
+	#Stops sending power to the GPIO pin, turning the LED off
+	GPIO.output(num,GPIO.LOW)
+	
+	#Changes the variable to keep the program from repeating forever
+	n += 1
